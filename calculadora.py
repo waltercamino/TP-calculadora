d8535092f165d0a8dacc1c73b8a3cd1898211bfa
conichan <conichan139@gmail.com>
2022-06-09 17:30:32 -0300

    estructura inicial

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+while True: #bucle para repetir
+    # Menu
+    print ("""    -----------------------------------
+    |         MINI CALCULADORA        |
+    -----------------------------------
+                1 - Suma
+                2 - Resta
+                3 - Multiplicacion
+                4 - Division
+                0 - Salir""")
+    print ("Ingrese la operacion que desea realizar: ")
+    opcion = input ()
+    # Segun la opcion llamara a la funcion correspondiente
+    if opcion == "1":
+        print ("Si anda")
+        # Suma ()
+    elif opcion == "2":
+        print ("Si anda")
+        # Resta ()
+    elif opcion == "3":
+        print ("Si anda")
+        # Multiplicacion ()
+    elif opcion == "4":
+        print ("Si anda")
+        # Division ()
+    elif opcion == "0": # Si es cero saldra del programa
+        print ("¡¡Gracias Por Usar Nuestro Prgrama!!")
+        break 
+    else: print ("""Usted a ingresado una opcion no valida\nIngrese otra por favor""")# Opcion no valida de entrada    
